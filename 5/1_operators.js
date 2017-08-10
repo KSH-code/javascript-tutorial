a5db7f565f254ed1f9a6ac3b51c64d123d11a866
Kim SeongHoon <tjdgnsqn133@gmail.com>
2017-08-10 13:56:20 +0900

    2017-08-10

---
+++ 5/1_operators.js
@@ -0,0 +1,38 @@
+/**
+ * @author [KSH-Code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-08-10 01:46:05
+ * @modify date 2017-08-10 01:46:05
+ * @desc [description]
+*/
+/**
+ * @author [KSH-Code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-08-10 01:30:04
+ * @modify date 2017-08-10 01:30:04
+ * @desc [대입 연산자]
+*/
+var x = 1
+console.log(x)//1
+x+=1
+console.log(x)//x + 1
+x-=1
+console.log(x)//x - 1
+x*=2
+console.log(x)//x * 2
+x/=2
+console.log(x)//2 | x
+x%=2
+console.log(x)//x mod 2
+x<<=1
+console.log(x)// 0001 -> 0010
+x>>=1
+console.log(x)// 0010 -> 0001
+x>>>=1
+console.log(x)// 0001 -> 0000 (부호없는)
+x&=1
+console.log(x)// 0000 같은값이 없음 0000 = 0
+x^=1
+console.log(x)// 0000 0001 xor = 1
+x|=1
+console.log(x)// 0000 0001 or = 1
---
+++ 5/2_operators.js
@@ -0,0 +1,25 @@
+/**
+ * @author [KSH-Code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-08-10 01:54:24
+ * @modify date 2017-08-10 01:54:24
+ * @desc [비교 연산자]
+*/
+var x = 1
+console.log(x=='1')
+console.log(x==1)
+console.log()
+console.log(x!='1')
+console.log(x!=1)
+console.log()
+console.log(x==='1')
+console.log(x===1)
+console.log()
+console.log(x!=='1')
+console.log(x!==1)
+console.log()
+console.log(x<1)
+console.log(x>1)
+console.log()
+console.log(x<=1)
+console.log(x>=1)
