c55189b6d9ac64b128276ad57bc2fbd0b120e71c
Kim SeongHoon <tjdgnsqn133@gmail.com>
2017-08-10 14:06:19 +0900

    2017-08-10

---
+++ 5/4_operators.js
@@ -0,0 +1,21 @@
+/**
+ * @author [KSH-Code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-08-10 02:04:37
+ * @modify date 2017-08-10 02:04:37
+ * @desc [비트단위 연산자]
+*/
+var x = 15
+console.log(x&10)
+console.log()
+console.log(x|12)
+console.log()
+console.log(x^11)
+console.log()
+console.log(~x)
+console.log()
+console.log(x<<2)
+console.log()
+console.log(x>>2)
+console.log()
+console.log(x>>>2)
