55f484804e1fc63697401f9af0850e5598a4aed4
김성훈 <gimseonghun@192.168.nate.com>
2017-08-21 16:17:43 +0900

    2017-08-21

---
+++ 8/1.js
@@ -0,0 +1,10 @@
+/**
+ * @author [KSH-Code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-08-21 04:12:51
+ * @modify date 2017-08-21 04:12:51
+ * @desc [description]
+ */
+var arr1 = new Array(element0, element1, elementN);
+var arr2 = Array(element0, element1, elementN);
+var arr3 = [element0, element1, elementN];
